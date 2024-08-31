141c8495eef262a8dd2a28d786a809b7ed63890a
christianjaylina <christianjay.lina@hcdc.edu.ph>
2024-08-31 11:22:06 +0800

    add theme

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const body = document.getElementsByTagName('body')[0];
 
 const DEFAULT_THEME = '';
 const DARK_THEME = 'dark-theme';
+const PERSONAL_THEME = 'personal-theme';
 
 function updateTheme(theme){
     body.classList.forEach((key) =>{
